a0e920db83feeef6acbbf50d5d87114d4c2206c1
Pushpal Patil <gauripushp@gmail.com>
2025-06-23 22:59:19 -0400

    added interface

--- src/app/selectionsort.test.ts
+++ src/app/selectionsort.test.ts
@@ -4,3 +4,12 @@ import { selectionsort } from './selectionsort'
 test('selection sort successfully sorts an array of small integers', () => {
     expect(selectionsort([4, 3, 2, 16, 1, 3, 15, 2, 15, 9])).toEqual([ 1, 2, 2, 3, 3, 4, 9, 15, 15, 16 ])
 })
+
+
+test('selection sort successfully returns undefined for empty array', () => {
+    expect(selectionsort([])).toEqual(undefined)
+})
+
+test('selection sort successfully returns the single element for a single element array', () => {
+    expect(selectionsort([2])).toEqual([2])
+})
---
+++ src/app/selectionsortInterface.ts
@@ -0,0 +1,16 @@
+
+export interface SelectionSortState{
+    // what data do I need
+    // what visual indicators do I need
+    // what metadata helps users understand what's happening
+    
+    inputArray : number[];
+    arrayLength : number;
+    indexI : number;
+    indexJ : number;
+    minimum : number;
+    outputArray : number[];
+    temp : number;
+    swap : number;
+}
+
