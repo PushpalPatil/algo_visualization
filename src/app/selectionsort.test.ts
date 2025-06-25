c63898b985b2bd7a62e210c87f92dd725119d989
Pushpal Patil <gauripushp@gmail.com>
2025-06-25 14:29:43 -0400

    selection sort with states

--- src/app/selectionsort.test.ts
+++ src/app/selectionsort.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import { selectionsort } from './selectionsort'
+import { selectionsort } from '../lib/algorithms/selectionsort'
 
 test('selection sort successfully sorts an array of small integers', () => {
     expect(selectionsort([4, 3, 2, 16, 1, 3, 15, 2, 15, 9])).toEqual([ 1, 2, 2, 3, 3, 4, 9, 15, 15, 16 ])
---
+++ src/components/playback/animatedsort.tsx
@@ -0,0 +1,19 @@
+import { SelectionSortVisualizer } from "../visualizers/selectionSortVisualizer";
+
+export function AnimatedSort() {
+  const [frames, setFrames] = useState<YourAlgorithmState[]>([]);
+  const [currentFrame, setCurrentFrame] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  
+  const runSort = (inputArray: number[]) => {
+    // Use your chosen approach to get all frames
+    // Set them in state
+  };
+  
+  return (
+    <div>
+      <SelectionSortVisualizer state={frames[currentFrame]} />
+      {/* Add controls */}
+    </div>
+  );
+}
