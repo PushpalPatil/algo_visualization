6fb022fba681e1c88df07a4e7625f0027f660b8a
Pushpal Patil <gauripushp@gmail.com>
2025-06-25 17:27:09 -0400

    next button and basic visualization - literally just seeing array

--- src/components/visualizers/selectionSortVisualizer.tsx
+++ src/components/visualizers/selectionSortVisualizer.tsx
@@ -1,28 +1,28 @@
 import { SelectionSortState } from "@/lib/algorithms/selectionsort"
-import {motion, AnimateSharedLayout} from 'framer-motion'
 
 
 export function SelectionSortVisualizer({ state }: { state: SelectionSortState }) {
 
     // does this automatically assign all relative state values to the respective values?
     // not sure, but the error went away
-    const {array, i , j, minIndex, swapped} = state
+    const { array, i, j, minIndex, swapped } = state
 
     // now what do i do with the elements
     // I need to use them to change states in some manner.
     // how do I know which state to change
     // do I need a map: to store keys and their values? 
     // a map of what? How do I define the value? : 
-    
+
+    // {/* Render the array */ }
+    //     {/* Show visual indicators */ }
+    //     {/* Display current operation */ }
+
 
     return (
-        {/* Render the array */ }
-        {/* Show visual indicators */ }
-        {/* Display current operation */ }
 
         <div>
             <h1>
-              
+                {JSON.stringify(state)}
             </h1>
         </div>
     )
