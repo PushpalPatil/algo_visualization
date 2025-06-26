37ac386eba6cf77b37555818a7c4a26fa2efa679
Pushpal Patil <gauripushp@gmail.com>
2025-06-26 15:25:18 -0400

    commented out animatedsort

--- src/components/playback/animatedsort.tsx
+++ src/components/playback/animatedsort.tsx
@@ -1,22 +1,22 @@
-"use client"
+// "use client"
 
-import { SelectionSortVisualizer } from "../visualizers/selectionSortVisualizer";
+// import { SelectionSortVisualizer } from "../visualizers/selectionSortVisualizer";
 
 
-export function AnimatedSort() {
-  const [frames, setFrames] = useState<YourAlgorithmState[]>([]);
-  const [currentFrame, setCurrentFrame] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
+// export function AnimatedSort() {
+//   const [frames, setFrames] = useState<YourAlgorithmState[]>([]);
+//   const [currentFrame, setCurrentFrame] = useState(0);
+//   const [isPlaying, setIsPlaying] = useState(false);
   
-  const runSort = (inputArray: number[]) => {
-    // Use your chosen approach to get all frames
-    // Set them in state
-  };
+//   const runSort = (inputArray: number[]) => {
+//     // Use your chosen approach to get all frames
+//     // Set them in state
+//   };
   
-  return (
-    <div>
-      <SelectionSortVisualizer state={frames[currentFrame]} />
-      {/* Add controls */}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <SelectionSortVisualizer state={frames[currentFrame]} />
+//       {/* Add controls */}
+//     </div>
+//   );
+// }
